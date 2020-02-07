e79a02d80bb784b4541b3b978b20a9acb6917f2f
piapip <tho.ph153619@sis.hust.edu.vn>
2020-02-07 17:01:57 +0700

    Reordering and update the gradient descent formula.

--- GradientDescent/nextThetaIteration.m
+++ GradientDescent/nextThetaIteration.m
@@ -1,6 +1,5 @@
 function nextTheta = nextThetaIteration(X, y, theta, alpha)
   m = size(X)(1)
-  % sumMass = hypo(X, theta) - y
   sumMass = X' * (X * theta - y)
   nextTheta = theta - (alpha/m * sumMass)
 endfunction
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 Learning Machine Learning from Andrew Ng course on coursera. Storing stuffs in here.
+
+nextTheta = theta - (alpha/m * sumMass) <br/>
+% m = size(X)(1) <br/>
+% sumMass = X' * (X * theta - y) <br/>
